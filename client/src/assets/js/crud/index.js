c8e79372622ab55814bd8c2f2238013759edaf3d
User Name <artur.it2914@gmail.com>
2019-12-10 19:49:30 +0200

    reduce code

--- client/src/assets/js/crud/index.js
+++ client/src/assets/js/crud/index.js
@@ -1,32 +1,28 @@
 import {serverIP, serverPort, tasksPath, taskPath} from "../constants/rest_path/index.js";
 
 
-export const getList = async () => {
-    const response = await fetch(`http://${serverIP}:${serverPort}${tasksPath}`);
+const fetchJson = async (url, options) => {
+    const response = await fetch(url, options);
     return response.json();
 };
 
-export const createTask = async (task) => {
-    const response = await fetch(`http://${serverIP}:${serverPort}${taskPath}`, {
+export const getList = () => fetchJson(`http://${serverIP}:${serverPort}${tasksPath}`);
+
+export const createTask = (task) =>
+    fetchJson(`http://${serverIP}:${serverPort}${taskPath}`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json',},
         body: JSON.stringify(task),
     });
-    return response.json();
-};
 
-export const updateTask = async (id, isDone) => {
-    const response = await fetch(`http://${serverIP}:${serverPort}${taskPath}/${id}`, {
+export const updateTask = (id, isDone) =>
+    fetchJson(`http://${serverIP}:${serverPort}${taskPath}/${id}`, {
         method: "PUT",
         headers: {'Content-Type': 'application/json',},
         body: JSON.stringify({isDone: !isDone}),
     });
-    return response.json();
-};
 
-export const deleteTask = async (id) => {
-    const response = await fetch(`http://${serverIP}:${serverPort}${taskPath}/${id}`, {
+export const deleteTask = (id) =>
+    fetchJson(`http://${serverIP}:${serverPort}${taskPath}/${id}`, {
         method: 'DELETE',
     });
-    return response.json();
-};
--- client/src/assets/js/index.js
+++ client/src/assets/js/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import * as restPath from './constants/rest_path/index.js'
-
 import {
     taskInputElem,
     addTaskButtonElem,
@@ -12,55 +10,36 @@ import {
 import createTaskListItemElem from './task/index.js';
 import {getList, createTask, deleteTask, updateTask} from "./crud/index.js";
 
-const onUpdateClick = async e => {
-    const id = e.currentTarget.dataset.id;
 
-    const isDone = e.currentTarget.dataset.isDone === 'true';
+const addTask = task => createTaskListItemElem(task, onUpdateClick, onDeleteClick);
 
-    const task = await updateTask(id, isDone);
-    const newTask = createTaskListItemElem(task, onUpdateClick, onDeleteClick);
-    const oldChild = document.getElementById(id);
-
-    tasksListElem.replaceChild(newTask, oldChild)
+const onUpdateClick = async ({currentTarget: {dataset: {id, isDone}}}) => {
+    tasksListElem.replaceChild(
+        addTask(await updateTask(id, isDone === 'true')),
+        document.getElementById(id)
+    )
 };
 
-const onDeleteClick = async e => {
-    const id = e.currentTarget.dataset.id;
-
-    const data = await deleteTask(id);
-
-    if (data) {
-        const oldChild = document.getElementById(id);
-        oldChild.remove();
+const onDeleteClick = async ({currentTarget: {dataset: {id}}}) => {
+    if (await deleteTask(id)) {
+        document.getElementById(id).remove();
     }
-};
-
-const onClickCreate = async value => {
-    const task = await createTask({value});
-    tasksListElem.prepend(createTaskListItemElem(task, onUpdateClick, onDeleteClick));
-    taskInputElem.value = "";
 };
 
 addTaskButtonElem.onclick = async function (e) {
     const {value} = taskInputElem;
     if (value) {
-        const createResult = await onClickCreate(value);
-        console.log(createResult)
+        tasksListElem.prepend(addTask(await createTask({value})));
+        taskInputElem.value = "";
     }
 };
-
 
 resetInputButtonElem.onclick = function (e) {
     taskInputElem.value = "";
 };
 
-const fillList = (list) => {
-    for (const task of list) {
-        tasksListElem.appendChild(createTaskListItemElem(task, onUpdateClick, onDeleteClick));
-    }
-};
-
 window.onload = async () => {
-    const list = await getList();
-    fillList(list);
+    for (const task of await getList()) {
+        tasksListElem.appendChild(addTask(task));
+    }
 };
